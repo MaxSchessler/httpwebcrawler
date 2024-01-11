7ab918410f6fcceb68256f70ce0f7f605e95178a
Max Schessler <schessler.max@gmail.com>
2024-01-11 17:55:20 -0600

    added recursive - with bugs

--- crawl.js
+++ crawl.js
@@ -44,27 +44,54 @@ function normalizeURL(urlString) {
     return hostPath;
 }
 
-async function crawlPage(currentURL, baseURL) {
+async function crawlPage(baseURL, currentURL, pages) {
     console.log("Crawling " + currentURL);
+
+    // check if currentURL is on the same site
+    const baseURLObj = new URL(baseURL);
+    const currentURLObj = new URL(baseURL);
+
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages;
+    }
+
+    // check if we already crawled this page
+    // get normalized version
+    // check if normalizedCurrentURL is in pages object
+    const normalizedCurrentURL = normalizeURL(currentURL);
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizeURL] ++;
+        return pages;
+    }
+
+    // not visited this currentURL
+    pages[normalizedCurrentURL] = 1;
 
     try {
         const responce = await fetch(currentURL);
         // make sure responce status is not errored out
         if (responce.status > 399) {
             console.log(`ERROR IN FETCH: STATUS: ${ responce.status}`);
-            return;
+            return pages;
         }
         // get content type and make sure it is html
         const contentType = responce.headers.get("content-type");
         if (!contentType.includes("text/html")) {
             console.log(`NON HTML RESPONCE: ${contentType} ON ${currentURL}`);
-            return;
+            return pages;
         }
 
-        const links = getURLsFromHTML(await responce.text(), baseURL);
-        console.log(links);
+        // get html, get links from html body
+        const htmlBody = await responce.text();
+        const links = getURLsFromHTML(htmlBody, baseURL);
+        // recurrsively call crawlpages
+        for (const nextURL of Object.entries(pages)) {
+            await 
+            crawlPage(baseURL, nextURL, pages);
+        }
 
-        console.log(hrefs);
+        return pages;
+
 
     } catch (err) {
         console.log(`ERROR ${err.message}`)
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require("./crawl.js");
 
-function main() {
+async function main() {
     if (process.argv.length < 3) {
         console.log("NO WEBSITE PROVIDED.");
         return;
@@ -11,8 +11,8 @@ function main() {
 
     const baseURL = process.argv[2];
     console.log(`CRAWLING ${baseURL} NOW...`);
-    crawlPage(baseURL, baseURL);
-
+    const pages = await crawlPage(baseURL, baseURL, {});
+    console.log(JSON.stringify(pages));
 }
 
 main();

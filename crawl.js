32cc1991e88dc7c42fa5959aecd27e9b0e48889d
Max Schessler <schessler.max@gmail.com>
2024-01-13 14:17:23 -0600

    finished

--- crawl.js
+++ crawl.js
@@ -1,106 +1,87 @@
-const {JSDOM} = require("jsdom");
-const { cursorTo } = require("readline");
+const { JSDOM } = require('jsdom')
 
-function getURLsFromHTML(htmlBody, baseURL) {
-    // base is base url of website we are crawling
-    // htmlBodyis html of website we are crawling
-    // returns araw of strings represending the urls on the web page
-    let urls = [];
-    const dom = new JSDOM(htmlBody);
-    const aTags = dom.window.document.querySelectorAll("a");
-    
-    for (const linkElement of aTags) {
-        if (linkElement.href.slice(0, 1) === "/") {
-            // this is a relative url
-            // append relative url to end of base url
-            try {
-                const url = new URL(`${baseURL}${linkElement.href}`);
-                urls.push(url.href);
-            } catch (error) {
-                console.log("ERROR: " + error.message);
-            }
-            
-        } else {
-            // this is a absolute url
-            try {
-                const url = new URL(linkElement.href);
-                urls.push(url.href);
-            } catch (error) {
-                console.log("ERROR: " + error.message);
-            }
-        }
+async function crawlPage(baseURL, currentURL, pages){
+  // if this is an offsite URL, bail immediately
+  const currentUrlObj = new URL(currentURL)
+  const baseUrlObj = new URL(baseURL)
+  if (currentUrlObj.hostname !== baseUrlObj.hostname){
+    return pages
+  }
+  
+  const normalizedURL = normalizeURL(currentURL)
+
+  // if we've already visited this page
+  // just increase the count and don't repeat
+  // the http request
+  if (pages[normalizedURL] > 0){
+    pages[normalizedURL]++
+    return pages
+  }
+
+  // initialize this page in the map
+  // since it doesn't exist yet
+  pages[normalizedURL] = 1
+
+  // fetch and parse the html of the currentURL
+  console.log(`crawling ${currentURL}`)
+  let htmlBody = ''
+  try {
+    const resp = await fetch(currentURL)
+    if (resp.status > 399){
+      console.log(`Got HTTP error, status code: ${resp.status}`)
+      return pages
     }
-    return urls;
+    const contentType = resp.headers.get('content-type')
+    if (!contentType.includes('text/html')){
+      console.log(`Got non-html response: ${contentType}`)
+      return pages
+    }
+    htmlBody = await resp.text()
+  } catch (err){
+    console.log(err.message)
+  }
+
+  const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+  for (const nextURL of nextURLs){
+    pages = await crawlPage(baseURL, nextURL, pages)
+  }
+
+  return pages
 }
 
-
-function normalizeURL(urlString) {
-    const urlObj = new URL(urlString);
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
-    // check/remove trailing /
-    if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
-        return hostPath.slice(0, -1);
-    } 
-    return hostPath;
+function getURLsFromHTML(htmlBody, baseURL){
+  const urls = []
+  const dom = new JSDOM(htmlBody)
+  const aElements = dom.window.document.querySelectorAll('a')
+  for (const aElement of aElements){
+    if (aElement.href.slice(0,1) === '/'){
+      try {
+        urls.push(new URL(aElement.href, baseURL).href)
+      } catch (err){
+        console.log(`${err.message}: ${aElement.href}`)
+      }
+    } else {
+      try {
+        urls.push(new URL(aElement.href).href)
+      } catch (err){
+        console.log(`${err.message}: ${aElement.href}`)
+      }
+    }
+  }
+  return urls
 }
 
-async function crawlPage(baseURL, currentURL, pages) {
-    console.log("Crawling " + currentURL);
-
-    // check if currentURL is on the same site
-    const baseURLObj = new URL(baseURL);
-    const currentURLObj = new URL(baseURL);
-
-    if (baseURLObj.hostname !== currentURLObj.hostname) {
-        return pages;
-    }
-
-    // check if we already crawled this page
-    // get normalized version
-    // check if normalizedCurrentURL is in pages object
-    const normalizedCurrentURL = normalizeURL(currentURL);
-    if (pages[normalizedCurrentURL] > 0) {
-        pages[normalizeURL] ++;
-        return pages;
-    }
-
-    // not visited this currentURL
-    pages[normalizedCurrentURL] = 1;
-
-    try {
-        const responce = await fetch(currentURL);
-        // make sure responce status is not errored out
-        if (responce.status > 399) {
-            console.log(`ERROR IN FETCH: STATUS: ${ responce.status}`);
-            return pages;
-        }
-        // get content type and make sure it is html
-        const contentType = responce.headers.get("content-type");
-        if (!contentType.includes("text/html")) {
-            console.log(`NON HTML RESPONCE: ${contentType} ON ${currentURL}`);
-            return pages;
-        }
-
-        // get html, get links from html body
-        const htmlBody = await responce.text();
-        const links = getURLsFromHTML(htmlBody, baseURL);
-        // recurrsively call crawlpages
-        for (const nextURL of Object.entries(pages)) {
-            await 
-            crawlPage(baseURL, nextURL, pages);
-        }
-
-        return pages;
-
-
-    } catch (err) {
-        console.log(`ERROR ${err.message}`)
-    }
-
+function normalizeURL(url){
+  const urlObj = new URL(url)
+  let fullPath = `${urlObj.host}${urlObj.pathname}`
+  if (fullPath.length > 0 && fullPath.slice(-1) === '/'){
+    fullPath = fullPath.slice(0, -1)
+  }
+  return fullPath
 }
 
 module.exports = {
-    normalizeURL,
-    getURLsFromHTML,
-    crawlPage
+  crawlPage,
+  normalizeURL,
+  getURLsFromHTML
 }
